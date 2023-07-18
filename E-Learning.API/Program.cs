c063a6ab7b31f2e20a09c0d878754ea4abb68d55
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2023-07-18 11:41:10 -0700

    add Dependecy Injection in Program.cs

--- E-Learning.API/Program.cs
+++ E-Learning.API/Program.cs
@@ -1,8 +1,13 @@
+using E_Learning.Logics.Repostiory;
+using E_Learning.Logics.Repostiory.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
