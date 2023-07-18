7309d3541f398b6f571f9d6f339aad69d055a35a
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2023-07-18 11:21:48 -0700

    create new model -- AccountModel.cs

---
+++ E-Learning.Logics/Models/AccountModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Logics.Models
+{
+    public class AccountModel
+    {
+        public int id {  get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }   
+        public DateTime? tanggal_daftar { get; set; }
+        public int id_peran { get; set; }
+    }
+}
