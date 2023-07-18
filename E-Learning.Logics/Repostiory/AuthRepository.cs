93d0bc86d6be0649e9020cf98205467048cf88c4
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2023-07-18 11:19:23 -0700

    implement interface

---
+++ E-Learning.Logics/Repostiory/AuthRepository.cs
@@ -0,0 +1,29 @@
+﻿using E_Learning.Logics.Repostiory.Interface;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Logics.Repostiory
+{
+    public class AuthRepository: IAuthRepository
+    {
+        private readonly string? connection;
+        public AuthRepository(IConfiguration configuration)
+        {
+            connection = configuration.GetConnectionString("E-Learning");
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SignUp(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
