529232d100c3e0ff62084d9c152050fe518642de
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2023-07-18 11:33:20 -0700

    create AccountController

---
+++ E-Learning.API/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using E_Learning.Logics.Repostiory.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Learning.API.Controllers
+{
+    [Route("E-learningAPI/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+        public AccountController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAccounts()
+        {
+            var res = _accountRepository.GetAllAccounts();
+            return Ok(res);
+        }
+    }
+}
