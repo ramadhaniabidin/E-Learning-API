682a2f8fc38163caeefa2b132a502952822bf789
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2023-07-18 11:13:15 -0700

    create interface for Login and Sign Up

---
+++ E-Learning.Logics/Repostiory/Interface/IAuthRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Logics.Repostiory.Interface
+{
+    public interface IAuthRepository
+    {
+        public bool Login(string username, string password);
+        public bool SignUp(string username, string password);
+    }
+}
