4c03bee72e0f237d4baf75a7ad5a97c80fd9c30d
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-19 14:25:33 +0700

    add interface IAccountDetailRepository

---
+++ E-Learning.Logics/Repostiory/Interface/IAccountDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using E_Learning.Logics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Logics.Repostiory.Interface
+{
+    public interface IAccountDetailRepository
+    {
+        public string InsertUpdateDetail(AccountDetailModel accountDetail);
+    }
+}
