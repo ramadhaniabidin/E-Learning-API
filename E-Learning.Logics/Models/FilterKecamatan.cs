dc69764273be4631150485513af5d764c430d4e0
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-24 20:06:21 +0700

    add model to filter kecamatan

---
+++ E-Learning.Logics/Models/FilterKecamatan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace E_Learning.Logics.Models
+{
+    public class FilterKecamatan
+    {
+        [JsonPropertyName("provinsi")]
+        public string? provinsi { get; set; }
+
+        [JsonPropertyName("kabupaten")]
+        public string? kabupaten { get; set; }
+
+        [JsonPropertyName("kecamatan")]
+        public string? kecamatan { get; set; }
+    }
+}
